8341a073a25d512b332537ba06df59dcaf9af945
Mark Mensah <kwakumensahj@gmail.com>
2021-08-10 12:35:56 +0100

    fixed error import for store

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Store } from './store/store';
+import Store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
