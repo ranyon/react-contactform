5d20d9654933c18da1a2e0670b3cd074fc0c595c
Mark Mensah <kwakumensahj@gmail.com>
2021-08-24 12:34:30 +0100

    added edit and delete

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -8,7 +8,30 @@ const usersReducer = (state = initialState, action)=> {
    switch (action.type) {
        case "ADD_USER":
            return  { ...state.users, users: [...state.users, action.payload] };
-        
+
+           case "EDIT_USER": 
+           const updatedUser = state.users.map (
+               (each_user)=> {
+                     if (each_user.id === action.user_id) {
+                         return {...each_user, ...action.updatedUser}
+                     } else {
+                         return each_user
+                     }
+               }
+           ) 
+              return {...state, users: updatedUser}
+ 
+        case "DELETE_USER":
+     
+         let notDeletedUsers = state.users.filter(
+             (each_item_in_array)=> {
+                 
+                 return each_item_in_array.id !== action.payload
+                console.log(action.payload)
+             })
+ 
+             return {users: notDeletedUsers }
+            
    
        default:
            return state;
