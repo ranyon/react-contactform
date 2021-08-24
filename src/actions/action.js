cfdb5af01f7f5ec6f76ccc53cbf52694cdabd0cb
Mark Mensah <kwakumensahj@gmail.com>
2021-08-24 12:32:24 +0100

    added edit and delete

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,4 +1,6 @@
 export const ADD_USER = "ADD_USER"
+export const EDIT_USER = 'EDIT_USER'
+export const DELETE_USER = 'DELETE_USER'
 
 export const addUser = (user) => {
     console.log("addUser", user);
@@ -6,5 +8,22 @@ export const addUser = (user) => {
     return {
         type: ADD_USER,
         payload: user
+    }
+}
+
+export const EditUser = (user_id, updatedUser) =>{
+    updatedUser.id = Math.random().toString();
+    return{
+        type: EDIT_USER,
+        user_id : user_id,
+        updatedUser : updatedUser
+    }
+}
+
+
+export const DeleteUser = (id) =>{
+    return{
+        type: DELETE_USER,
+        payload: id
     }
 }
